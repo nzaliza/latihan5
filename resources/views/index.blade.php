8ff541b204d465654dcbdecf962fd14d6a4632bb
nzaliza <nzaliza@gmail.com>
2023-03-13 10:50:22 +0800

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+<body>
+    <h1>My nama is Lizz</h1>
+    <p>I work at KSM</p>
+    
+</body>
+
+    </head>
